ac3fd7fd13e4f5bb21218e9a40ecb1aa584db052
russmc <60695892+russmc@users.noreply.github.com>
2022-07-08 16:18:43 +0100

    feat: added simple toggle switch

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import ToggleSwitch from './components/Toggle';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <ToggleSwitch />
     </div>
   );
 }
