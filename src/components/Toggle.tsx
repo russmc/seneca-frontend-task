26cf492043912fcf4f06f7eea7a064a5a0e0d372
russmc <60695892+russmc@users.noreply.github.com>
2022-07-10 13:57:53 +0100

    feat: added option props to switch component

--- src/components/Toggle.tsx
+++ src/components/Toggle.tsx
@@ -4,13 +4,18 @@ import './Toggle.css'
 interface SwitchProps {
     isToggled: boolean;
     onToggle: React.ChangeEventHandler<HTMLInputElement>;
+    options?: Array<string> | null;
 }
 
-function Switch({ isToggled, onToggle }: SwitchProps) {
+function Switch({ isToggled, onToggle, options }: SwitchProps) {
     return (
         <label className="switch">
             <input type="checkbox" checked={isToggled} onChange={onToggle} />
             <span className="slider rounded" />
+            <div className="options-container">
+                { options && <div className="option">{options[0]}</div> }
+                { options && <div className="option">{options[1]}</div> }
+            </div>
         </label>
     );
 }
@@ -18,7 +23,13 @@ function Switch({ isToggled, onToggle }: SwitchProps) {
 export default function ToggleSwitch() {
     const [isToggled, setToggle] = useState(false);
 
+    const parsedOptions = ["option 1", "option 2 much longer text blah blah"]
+
     return (
-        <Switch isToggled={isToggled} onToggle={() => {setToggle(!isToggled)}} />
+        <Switch 
+            isToggled={isToggled} 
+            onToggle={() => {setToggle(!isToggled)}}
+            options={parsedOptions}
+        />
     );
 }
