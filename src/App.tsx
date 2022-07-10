3da6b46167ebbe7999bf1eff27b8cac25b11ed59
russmc <60695892+russmc@users.noreply.github.com>
2022-07-10 18:15:00 +0100

    feat: added options class

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import './App.css';
 import ToggleSwitch from './components/Toggle';
+import Options from './options';
 
 function App() {
+  const q1Options = new Options(
+    'q1 option a',
+    'q1 option b'
+  )
+  
   return (
     <div className="App">
-      <ToggleSwitch />
+      <ToggleSwitch options={q1Options} />
     </div>
   );
 }
--- src/components/Toggle.tsx
+++ src/components/Toggle.tsx
@@ -1,5 +1,6 @@
-import React, {useState} from "react";
-import './Toggle.css'
+import React, {useEffect, useState} from "react";
+import './Toggle.css';
+import Options from '../options';
 
 interface SwitchProps {
     isToggled: boolean;
@@ -13,17 +14,23 @@ function Switch({ isToggled, onToggle, options }: SwitchProps) {
             <input type="checkbox" checked={isToggled} onChange={onToggle} />
             <span className="slider rounded click-area" />
             <div className="options-container click-area">
-                { options && <div className="option click-area">{options[0]}</div> }
-                { options && <div className="option click-area">{options[1]}</div> }
+                { options?.map((option) => <div className="option click-area">{option}</div>) }
             </div>
         </label>
     );
 }
 
-export default function ToggleSwitch() {
-    const [isToggled, setToggle] = useState(false);
+interface ToggleSwitchProps {
+    options: Options;
+}
 
-    const parsedOptions = ["option 1", "option 2 much longer text blah blah"]
+export default function ToggleSwitch({ options }: ToggleSwitchProps) {
+    const [isToggled, setToggle] = useState<boolean>(false);
+    const [parsedOptions, setParsedOptions] = useState<Array<string> | null>(null);
+
+    useEffect(() => {
+        setParsedOptions(options.shuffle())
+    }, [options])
 
     return (
         <Switch 
